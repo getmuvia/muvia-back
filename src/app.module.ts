94456bdbbe7b98492e4727c3090c6430329b313f
jherson <rodriguezpaul98@gmail.com>
2025-12-14 00:25:28 -0400

    feat(config): typeorm config with postgres implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,21 @@ import { ProductsModule } from './modules/products/products.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { FilesController } from './modules/files/files.controller';
 import { FilesService } from './modules/files/files.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [AuthModule, UsersModule, ProductsModule, CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeOrmConfig,
+    }),
+    AuthModule, UsersModule, ProductsModule, CategoriesModule],
   controllers: [AppController, FilesController],
   providers: [AppService, FilesService],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,26 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig = async (
+  configService: ConfigService,
+): Promise<TypeOrmModuleOptions> => {
+  const isProduction = configService.get<string>('NODE_ENV') === 'production';
+
+  return {
+    type: 'postgres',
+    
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+
+    autoLoadEntities: true, 
+    
+    synchronize: !isProduction, 
+
+    ssl: isProduction ? { rejectUnauthorized: false } : false,
+    
+    logging: !isProduction, 
+  };
+};
