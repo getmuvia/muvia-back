5686f0afd583544e6aec7e4b9dffd58143534780
jherson <rodriguezpaul98@gmail.com>
2025-12-14 00:27:23 -0400

    implement environment validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,13 @@ import { FilesService } from './modules/files/files.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: envValidationSchema,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
