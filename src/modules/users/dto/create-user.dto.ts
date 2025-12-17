bad088ec188c152f523da0c7fa030b0b4daa7558
jherson <rodriguezpaul98@gmail.com>
2025-12-17 18:46:27 -0400

    feat(users): add DTOs for user creation with validation

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { UserRole } from '../entities/user.entity';
+
+class CreateVendorProfileDto {
+  @IsString()
+  @IsNotEmpty()
+  businessName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+}
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  password: string;
+
+  @IsEnum(UserRole)
+  role: UserRole;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => CreateVendorProfileDto)
+  vendorProfile?: CreateVendorProfileDto;
+}
