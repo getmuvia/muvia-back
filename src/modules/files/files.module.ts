80dc8d50456caf50b889be6299e18222d5e9d02e
jherson <rodriguezpaul98@gmail.com>
2026-01-12 23:26:38 -0400

    feat(files): add google cloud storage provider to upload files

--- src/modules/files/files.module.ts
+++ src/modules/files/files.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 import { STORAGE_PROVIDER } from './interfaces/storage-provider.interface';
-import { LocalStorageProvider } from './providers/local-storage.provider';
+import { GoogleCloudStorageProvider } from './providers/google-storage.provider';
 
 @Module({
     imports: [ConfigModule],
@@ -12,7 +12,7 @@ import { LocalStorageProvider } from './providers/local-storage.provider';
         FilesService,
         {
             provide: STORAGE_PROVIDER,
-            useClass: LocalStorageProvider,
+            useClass: GoogleCloudStorageProvider,
         },
     ],
     exports: [FilesService],
