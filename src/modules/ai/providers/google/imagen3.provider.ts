2c12914a778337c8768bd4975a337811b6e38610
jherson <rodriguezpaul98@gmail.com>
2026-01-26 22:53:24 -0400

    change image provide model

--- src/modules/ai/providers/google/imagen3.provider.ts
+++ src/modules/ai/providers/google/imagen3.provider.ts
@@ -27,7 +27,7 @@ export class Imagen3Provider implements IImageGenerator {
     private readonly projectId: string;
     private readonly location: string;
 
-    private readonly MODEL_NAME = 'imagen-4.0-ultra-generate-001';
+    private readonly MODEL_NAME = 'imagen-3.0-generate-002';
 
     constructor(private readonly configService: ConfigService) {
         this.projectId = this.configService.get<string>('GCP_PROJECT_ID') ?? '';
