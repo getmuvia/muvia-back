49bf58c16a08f06e826f0dd77e638df888578cda
jherson <rodriguezpaul98@gmail.com>
2026-02-06 19:55:01 -0400

    fix(product): update product now contain assets

--- src/modules/products/dto/update-product.dto.ts
+++ src/modules/products/dto/update-product.dto.ts
@@ -1,6 +1,16 @@
 import { PartialType, OmitType } from '@nestjs/mapped-types';
+import { IsArray, IsOptional, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 import { CreateProductDto } from './create-product.dto';
+import { SyncProductAssetDto } from './sync-product-asset.dto';
 
 export class UpdateProductDto extends PartialType(
     OmitType(CreateProductDto, ['assets'] as const),
-) { }
+) {
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => SyncProductAssetDto)
+    @IsOptional()
+    assets?: SyncProductAssetDto[];
+}
+
